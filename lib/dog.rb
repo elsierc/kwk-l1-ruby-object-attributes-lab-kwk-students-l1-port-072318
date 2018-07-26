e0329f200e8ce4aabf0abcaccc932bb8731aef5d
elsierc <elsierc22@gmail.com>
2018-07-26 23:16:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # dog.rb
+class Dog
+
+  
+  def breed 
+    @breed 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def name = (name)
+    @name = name
+    
+  end
+    def breed = (breed)
+  @breed = breed
+  end 
+  
+end
+
+moose = Dog.new("Saint Brenard", "Moose")
+moose.name(gets.strip)
+puts "You just changed the name of your dog to #{new_name}!"
